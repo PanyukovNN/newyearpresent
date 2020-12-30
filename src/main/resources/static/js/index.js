fcb9a73293e97134114e8bb277843ac128239ade
Nickolay <zylex77@gmail.com>
2020-12-31 02:11:24 +0300

    Добавлено открытие подарка

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,5 +1,8 @@
+let mainText = $(".main-text");
+var bunny = $(".bunny");
+
 $(document).ready(function(){
-    // $(document).snowfall({image :"img/2.png", minSize: 10, maxSize:20, maxSpeed: 3, minSpeed: 1});
+    $(document).snowfall({image :"img/2.png", minSize: 10, maxSize:20, maxSpeed: 3, minSpeed: 1});
 
     var imagesNumber = 12;
 
@@ -9,8 +12,6 @@ $(document).ready(function(){
     }
 
     shuffle(arr)
-
-    console.log(arr)
 
     var backstretchData = [];
 
@@ -27,10 +28,21 @@ function shuffle(array) {
 
 function getTimeRemaining(endtime) {
     var t = Date.parse(endtime) - Date.parse(new Date());
+
     var seconds = Math.floor((t / 1000) % 60);
     var minutes = Math.floor((t / 1000 / 60) % 60);
     var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
     var days = Math.floor(t / (1000 * 60 * 60 * 24));
+
+    if (t < 0) {
+        seconds = 0;
+        minutes = 0;
+        hours = 0;
+        days = 0;
+
+        mainText.html("Открывай подарок, Солнце мое <br>&#129392;&#128151;&#127873;&#128151;&#128522;");
+    }
+
     return {
         'total': t,
         'days': days,
@@ -53,9 +65,6 @@ function initializeClock(id, endtime) {
             "<span class='timer-number'>" + ('0' + t.minutes).slice(-2) + "</span> м. " +
             "<span class='timer-number'>" + ('0' + t.seconds).slice(-2) + "</span> сек.";
 
-        // var wallpaperIndex = parseInt(new Date().getMinutes() / 6);
-        // $(".main-image").css("background-image", "url(\"../img/wallpaper-" + wallpaperIndex + ".jpg\")");
-
         if (t.total <= 0) {
             clearInterval(timeinterval);
         }
@@ -65,7 +74,7 @@ function initializeClock(id, endtime) {
     var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline="January 01 2021 00:30:00 GMT+0300";
+var deadline="January 01 2020 00:30:00 GMT+0300";
 var deadline = new Date(deadline);
 initializeClock('countdown', deadline);
 
@@ -74,6 +83,25 @@ var popup = $("#myPopup");
 var blockAnimation = false;
 
 present.on("click", function () {
+
+    var t = Date.parse(deadline) - Date.parse(new Date());
+    if (t <= 0) {
+        // Анимация открытия подарка
+        document.documentElement.style.setProperty('--animate-duration', '10s');
+        present.removeClass("animate__pulse");
+        present.removeClass("animate__infinite");
+
+        present.addClass("animate__rollOut");
+
+        mainText.html("&#127880;&#127882;УРА!&#127881;&#10024;<br>Теперь нажми на зайку и тебе откроется карта, где лежит настоящий подарок  &#127872;&#128071;");
+
+        document.documentElement.style.setProperty('--animate-duration', '1s');
+        bunny.addClass("animate__pulse");
+
+        pride();
+        return;
+    }
+
     if (blockAnimation === false) {
         blockAnimation = true;
 
@@ -114,36 +142,67 @@ document.documentElement.style.setProperty('--animate-duration', '1.5s');
 
 
 
-var duration = 1 * 1000;
-var animationEnd = Date.now() + duration;
-var skew = 1;
 
-function randomInRange(min, max) {
-    return Math.random() * (max - min) + min;
-}
+var end = Date.now() + (15 * 1000);
 
-(function frame() {
-    // var timeLeft = animationEnd - Date.now();
-    var timeLeft = 1;
-    var ticks = Math.max(200, 500);
-    skew = Math.max(0.8, skew - 0.001);
+// go Buckeyes!
+var colors = ['#FF3314', '#018667'];
 
+function pride() {
     confetti({
-        particleCount: 1,
-        startVelocity: 0,
-        ticks: ticks,
-        gravity: 0.5,
-        origin: {
-            x: Math.random(),
-            // since particles fall down, skew start toward the top
-            y: (Math.random() * skew) - 0.2
-        },
-        colors: ['#ffffff'],
-        shapes: ['circle'],
-        scalar: randomInRange(0.4, 1)
+        particleCount: 2,
+        angle: 60,
+        spread: 55,
+        origin: { x: 0 },
+        colors: colors
+    });
+    confetti({
+        particleCount: 2,
+        angle: 120,
+        spread: 55,
+        origin: { x: 1 },
+        colors: colors
     });
 
-    if (timeLeft > 0) {
-        requestAnimationFrame(frame);
+    if (Date.now() < end) {
+        requestAnimationFrame(pride);
     }
-}());
+};
+
+
+
+
+
+// var duration = 1 * 1000;
+// var animationEnd = Date.now() + duration;
+// var skew = 1;
+//
+// function randomInRange(min, max) {
+//     return Math.random() * (max - min) + min;
+// }
+//
+// (function frame() {
+//     // var timeLeft = animationEnd - Date.now();
+//     var timeLeft = 1;
+//     var ticks = Math.max(200, 500);
+//     skew = Math.max(0.8, skew - 0.001);
+//
+//     confetti({
+//         particleCount: 1,
+//         startVelocity: 0,
+//         ticks: ticks,
+//         gravity: 0.5,
+//         origin: {
+//             x: Math.random(),
+//             // since particles fall down, skew start toward the top
+//             y: (Math.random() * skew) - 0.2
+//         },
+//         colors: ['#ffffff'],
+//         shapes: ['circle'],
+//         scalar: randomInRange(0.4, 1)
+//     });
+//
+//     if (timeLeft > 0) {
+//         requestAnimationFrame(frame);
+//     }
+// }());
