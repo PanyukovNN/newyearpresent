a259a7f03c496691e802623404960302f691d317
Nickolay <zylex77@gmail.com>
2020-12-31 12:59:48 +0300

    style improvements

--- src/main/resources/static/js/present.js
+++ src/main/resources/static/js/present.js
@@ -1,23 +1,23 @@
-// var duration = 60 * 1000;
-// var animationEnd = Date.now() + duration;
-// var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
-//
-// function randomInRange(min, max) {
-//     return Math.random() * (max - min) + min;
-// }
-//
-// var interval = setInterval(function() {
-//     var timeLeft = animationEnd - Date.now();
-//
-//     if (timeLeft <= 0) {
-//         return clearInterval(interval);
-//     }
-//
-//     var particleCount = 100 * (timeLeft / duration);
-//     // since particles fall down, start a bit higher than random
-//     confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
-//     confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
-// }, 1000);
+var duration = 60 * 1000;
+var animationEnd = Date.now() + duration;
+var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+var interval = setInterval(function() {
+    var timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+        return clearInterval(interval);
+    }
+
+    var particleCount = 100 * (timeLeft / duration);
+    // since particles fall down, start a bit higher than random
+    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+}, 1000);
 
 var mapScaled = false;
 
@@ -29,4 +29,8 @@ $(".map").on("click", function () {
         $(this).css("width", "150px");
         mapScaled = false;
     }
+})
+
+$(document).ready(function() {
+    $(document).snowfall({image: "img/2.png", minSize: 10, maxSize: 20, maxSpeed: 3, minSpeed: 1});
 })
